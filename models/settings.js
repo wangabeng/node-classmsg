fe9e51c81343039b0399a0ef0dc5d33f77315a7a
ben <wangabeng@163.com>
2017-07-23 12:18:08 +0800

    Update settings.js

--- models/settings.js
+++ models/settings.js
@@ -1,3 +1,3 @@
 module.exports={
-	'dburl':''dburl':'mongodb://wangabeng:abeng528@ds117913.mlab.com:17913/firstdatabase/classyy'
+	'dburl':'mongodb://wangabeng:abeng528@ds117913.mlab.com:17913/firstdatabase/classyy'
 }
